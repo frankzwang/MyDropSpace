24a4762815f75b7d9c6694404ff198e76e26b5f1
Robert Fidler <robertf224@yahoo.com>
2013-01-19 16:32:24 -0500

    Initial Commit

---
+++ MyDropSpace/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyDropSpace
+//
+//  Created by Robert Fidler on 1/19/13.
+//  Copyright (c) 2013 Robert Fidler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyDropSpace/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyDropSpace
+//
+//  Created by Robert Fidler on 1/19/13.
+//  Copyright (c) 2013 Robert Fidler. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
